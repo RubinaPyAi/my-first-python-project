794551685298392d9ec1c305937a385b53be1e60
RubinaPyAi <rubinahanfi24@gmail.com>
2025-03-12 21:37:23 +0530

    Palindrome program

---
+++ palindrome_prog.py
@@ -0,0 +1,7 @@
+# Given string is PALINDROME or not
+str1=input("Enter the string:")
+if (str1==str1[::-1]):
+    print("entered string is PALINDROME")
+else:
+    print("Entered string is not a PALINDROME")
+        
