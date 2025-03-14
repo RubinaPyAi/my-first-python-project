dd4f69587fbf248e50753660f4d48c83eb91ef4e
RubinaPyAi <rubinahanfi24@gmail.com>
2025-03-14 12:33:28 +0530

    adding third comment from main branch

--- print_star.py
+++ print_star.py
@@ -1,8 +1,6 @@
 # Program to print the right angled triangle using '*'
 for i in range(1,6):
     print("*"*i)
-<<<<<<< HEAD
 print ("star pattern generated")
-=======
 print("this comment is for Ram branch file")
->>>>>>> Ram
+print(" third comment from main branch")
